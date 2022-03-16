e2621c0fe271c2a3c3b1b4ca4d801778a406886b
hylee0824 <hoyeon.lee.0824@gmail.com>
2022-03-17 01:22:41 +0900

    갤러리 및 탑버튼 추가

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+const useGoogleDrive = true;
+const albumImgCount = 28;
 
 // 디데이
 var dDayholder = document.getElementById("d-day-holder");
@@ -13,4 +15,85 @@ if (gap < 0) {
 
 dDayholder.append(dday_text);
 
+setScreenSize();
 
+function setScreenSize() {
+    let vh = window.innerHeight * 0.01;
+
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+};
+  
+var dAlbumHolder = document.getElementById('album-holder');
+var lastAlbumImgIdx = 0;
+
+for (var i = 0; i < albumImgCount-1; i++) {
+    var thumbImgs = thumbImgLinks();
+
+    dAlbumHolder.innerHTML += `        
+    <a class="col-sm-4 col-4 p-3" data-bs-toggle="modal" data-bs-target="#modal-album" data-bs-slide-to="${i}">
+        <div class="square">
+            <img class="thumbnail lazy wow fadeIn" data-src="${thumbImgs[i+1]}" alt="">
+        </div>
+    </a>
+    `;
+    lastAlbumImgIdx = i;
+}
+
+function thumbImgLinks() {
+    var thumbImgs = {};
+
+    for (var i = 0; i < albumImgCount; i++) {
+        thumbImgs[i] = `images/gallery/${i+1}.jpg`;
+    }
+
+    return thumbImgs;
+}
+
+// Lazy
+document.addEventListener("DOMContentLoaded", function() {
+    var lazyloadImages = document.querySelectorAll("img.lazy.thumbnail");
+    var lazyloadThrottleTimeout;
+
+    function lazyload() {
+        if (lazyloadThrottleTimeout) {
+            clearTimeout(lazyloadThrottleTimeout);
+        }
+
+        lazyloadThrottleTimeout = setTimeout(function() {
+            var scrollTop = window.pageYOffset;
+            lazyloadImages.forEach(function(img) {
+                if (img.offsetTop < (window.innerHeight + scrollTop)) {
+                    img.src = img.dataset.src;
+                    img.classList.remove('lazy');
+                }
+            });
+            if (lazyloadImages.length == 0) {
+                document.removeEventListener("scroll", lazyload);
+                window.removeEventListener("resize", lazyload);
+                window.removeEventListener("orientationChange", lazyload);
+            }
+        }, 20);
+    }
+
+    document.addEventListener("scroll", lazyload);
+    window.addEventListener("resize", lazyload);
+    window.addEventListener("orientationChange", lazyload);
+});
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+var mybutton = document.getElementById("topBtn");
+
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        mybutton.classList.add('d-inline-flex');
+        mybutton.classList.remove('d-none');
+    } else {
+        mybutton.classList.remove('d-inline-flex');
+        mybutton.classList.add('d-none');
+    }
+}
