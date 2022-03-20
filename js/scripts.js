22e7e6f8c5635d0b35b8983231e6ea13af2cd9f1
hylee0824 <hoyeon.lee.0824@gmail.com>
2022-03-20 20:16:27 +0900

    이미지 수정

--- js/scripts.js
+++ js/scripts.js
@@ -45,9 +45,45 @@ for (var i = 0; i < albumImgCount; i++) {
 function thumbImgLinks() {
     var thumbImgs = {};
 
-    for (var i = 0; i < albumImgCount; i++) {
-        thumbImgs[i+1] = `images/gallery/${i+1}.jpg`;
-    }
+    thumbImgs[1] = `images/thumbnail/14.jpg`;
+    thumbImgs[2] = `images/thumbnail/15.jpg`;
+    thumbImgs[3] = `images/thumbnail/16.jpg`;
+    thumbImgs[4] = `images/thumbnail/17.jpg`;
+
+    thumbImgs[5] = `images/thumbnail/18.jpg`;
+    thumbImgs[6] = `images/thumbnail/9.jpg`;
+    thumbImgs[7] = `images/thumbnail/10.jpg`;
+    thumbImgs[8] = `images/thumbnail/11.jpg`;
+
+    thumbImgs[9] = `images/thumbnail/19.jpg`;
+    thumbImgs[10] = `images/thumbnail/12.jpg`;
+    thumbImgs[11] = `images/thumbnail/13.jpg`;
+    thumbImgs[12] = `images/thumbnail/20.jpg`;
+
+    thumbImgs[13] = `images/thumbnail/4.jpg`;
+    thumbImgs[14] = `images/thumbnail/1.jpg`;
+    thumbImgs[15] = `images/thumbnail/3.jpg`;
+    thumbImgs[16] = `images/thumbnail/2.jpg`;
+
+    thumbImgs[17] = `images/thumbnail/6.jpg`;
+    thumbImgs[18] = `images/thumbnail/7.jpg`;
+    thumbImgs[19] = `images/thumbnail/8.jpg`;
+    thumbImgs[20] = `images/thumbnail/5.jpg`;
+
+    thumbImgs[21] = `images/thumbnail/23.jpg`;
+    thumbImgs[22] = `images/thumbnail/22.jpg`;
+    thumbImgs[23] = `images/thumbnail/24.jpg`;
+    thumbImgs[24] = `images/thumbnail/25.jpg`;
+
+    thumbImgs[25] = `images/thumbnail/21.jpg`;
+    thumbImgs[26] = `images/thumbnail/26.jpg`;
+    thumbImgs[27] = `images/thumbnail/27.jpg`;
+    thumbImgs[28] = `images/thumbnail/28.jpg`;
+
+
+    // for (var i = 0; i < albumImgCount; i++) {
+    //     thumbImgs[i+1] = `images/thumbnail/${i+1}.jpg`;
+    // }
 
     return thumbImgs;
 }
@@ -144,7 +180,6 @@ myModalEl.addEventListener('show.bs.modal', function(event) {
 })
 
 
-
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
@@ -167,9 +202,44 @@ function scrollFunction() {
 function modalImgLinks() {
     var modalImgs = {};
 
-    for (var i = 0; i < albumImgCount; i++) {
-        modalImgs[i+1] = `images/gallery/${i+1}.jpg`;
-    }
+    modalImgs[1] = `images/gallery/14.jpg`;
+    modalImgs[2] = `images/gallery/15.jpg`;
+    modalImgs[3] = `images/gallery/16.jpg`;
+    modalImgs[4] = `images/gallery/17.jpg`;
+
+    modalImgs[5] = `images/gallery/18.jpg`;
+    modalImgs[6] = `images/gallery/9.jpg`;
+    modalImgs[7] = `images/gallery/10.jpg`;
+    modalImgs[8] = `images/gallery/11.jpg`;
+
+    modalImgs[9] = `images/gallery/19.jpg`;
+    modalImgs[10] = `images/gallery/12.jpg`;
+    modalImgs[11] = `images/gallery/13.jpg`;
+    modalImgs[12] = `images/gallery/20.jpg`;
+
+    modalImgs[13] = `images/gallery/4.jpg`;
+    modalImgs[14] = `images/gallery/1.jpg`;
+    modalImgs[15] = `images/gallery/3.jpg`;
+    modalImgs[16] = `images/gallery/2.jpg`;
+
+    modalImgs[17] = `images/gallery/6.jpg`;
+    modalImgs[18] = `images/gallery/7.jpg`;
+    modalImgs[19] = `images/gallery/8.jpg`;
+    modalImgs[20] = `images/gallery/5.jpg`;
+
+    modalImgs[21] = `images/gallery/23.jpg`;
+    modalImgs[22] = `images/gallery/22.jpg`;
+    modalImgs[23] = `images/gallery/24.jpg`;
+    modalImgs[24] = `images/gallery/25.jpg`;
+
+    modalImgs[25] = `images/gallery/21.jpg`;
+    modalImgs[26] = `images/gallery/26.jpg`;
+    modalImgs[27] = `images/gallery/27.jpg`;
+    modalImgs[28] = `images/gallery/28.jpg`;
+
+    // for (var i = 0; i < albumImgCount; i++) {
+    //     modalImgs[i+1] = `images/gallery/${i+1}.jpg`;
+    // }
 
     return modalImgs;
 }
